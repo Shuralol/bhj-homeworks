e7d9fc41e59ba8baedcda4ee08b087b1a99b6023
Shuralol <akinfeev09@bk.ru>
2023-02-20 12:53:35 +0700

    3 задание исправил

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -5,9 +5,12 @@ arr.forEach((item) => {
   item.addEventListener("click", (event) => {
     event.preventDefault();
     const text = event.target.textContent;
+    const li = event.target.closest(".dropdown");
     const parent = event.target.closest(".dropdown");
     const div = parent.querySelector(".dropdown__value");
     div.textContent = text;
+    const ul = li.querySelector("ul");
+    ul.classList.remove("dropdown__list_active");
   });
 });
 
