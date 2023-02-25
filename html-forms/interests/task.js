e3182f2366051815fadebcd916eb1bfebc8f178e
Shuralol <akinfeev09@bk.ru>
2023-02-25 20:20:24 +0700

    2 задание готово

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,9 +1,27 @@
-const checkbox1 = Array.from(document.querySelectorAll(".interest__check"))
-console.log(checkbox1)
-  
-checkbox1.forEach(elem => {
-    elem.addEventListener("click",(e) =>  {
-        
-    })
-    
+const checkbox1 = Array.from(document.querySelectorAll(".interest__check"));
+
+checkbox1.forEach((elem) => {
+  elem.addEventListener("click", (e) => {
+    const li = e.target.closest("LI");
+    const ul = li.querySelector("UL");
+    const check = e.target.checked;
+    if (ul) {
+      const input = ul.querySelectorAll("INPUT");
+      input.forEach((item) => (item.checked = check));
+    }
+    const liParent = li.closest("UL").closest("LI");
+    if (liParent) {
+      const arrInput = Array.from(
+        li.closest("UL").querySelectorAll(".interest__check")
+      );
+      if (arrInput) {
+        if (arrInput.every((item) => item.checked === check)) {
+          liParent.querySelector("INPUT").indeterminate = false;
+          liParent.querySelector("INPUT").checked = check;
+        } else {
+          liParent.querySelector("INPUT").indeterminate = true;
+        }
+      }
+    }
+  });
 });
