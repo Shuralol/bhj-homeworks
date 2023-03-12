cee6744debbb1074492fd8dacf2be3185883c2df
Shuralol <akinfeev09@bk.ru>
2023-03-12 17:32:08 +0700

    3 задание не готово

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,24 +1,13 @@
 /* const minusPlus = document.querySelector(
   ".product__quantity-controls"
 ).children;
-console.log(minusPlus[0]); */
+console.log(minusPlus[0]);
 
-/* const minusPlus1 = document.querySelectorAll(
-  ".product__quantity-controls"
-); */ /* .nextSibling */ /* console.log(minusPlus1); 
+const minusPlus1 = document.querySelectorAll(".product__quantity-controls");
+ console.log(minusPlus1);
 
- const aaa = minusPlus1.querySelectorAll(".product__quantity-control product__quantity-control_dec")
-console.log(aaa) 
 
-/*   const minusPlus2 = document.querySelector(
-    ".product__quantity-controls"
-  ).lastChild;
-  console.log(minusPlus2);
-
-const minus = document.querySelectorAll("product__quantity-control product__quantity-control_dec")
-console.log(minus) */
-
-/* minusPlus[0].addEventListener("click", function () {
+minusPlus[0].addEventListener("click", function () {
   console.log("минус клик");
   if (parseInt(minusPlus[1].innerText) > 1) {
     minusPlus[1].innerText = --minusPlus[1].innerText;
@@ -28,11 +17,14 @@ console.log(minus) */
 minusPlus[2].addEventListener("click", function () {
   console.log("плюс клик");
   minusPlus[1].innerText = ++minusPlus[1].innerText;
-});
- */
+}); */
 
-window.addEventListener("click", function (event) {
-  if (event.target) {
+
+
+
+
+ window.addEventListener("click", function (event) {
+  if (event.target/*тут должен быть dataset.и что то еще  */) {
     console.log("клик виндов");
 
     console.log(event.target);
@@ -40,10 +32,10 @@ window.addEventListener("click", function (event) {
     const counterWrapper = event.target.closest(".product__quantity-controls");
 
     const counter = counterWrapper.querySelector(".product__quantity-value");
-
+ console.log(counter)
     counter.innerText = ++counter.innerText;
   }
-  /* if(event.target) {
+  if(event.target/*тут должен быть dataset.и что то еще  */) {
     counter.innerText = --counter.innerText;
-  } */
-});
+  }
+}); 
